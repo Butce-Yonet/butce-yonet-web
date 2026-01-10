dea8194632874414f717597a2214de644367a8f9
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2026-01-10 23:04:01 +0300

    Mobilde dil seçim probleminin çözülmesi

--- src/components/header/language.vue
+++ src/components/header/language.vue
@@ -1,16 +1,12 @@
 <template>
     <li class="language-nav">
         <div class="translate_wrapper">
-            <div class="translate_wrapper " :class="{ 'active': isActive }">
+            <div class="translate_wrapper " :class="{ 'active': isActive }" @click="display()">
                 <div class="current_lang">
-                    <div class="lang"><i class="flag-icon" :class="this.langIcon"></i><span class="lang-txt"
-                            @click="display()">{{ langLangauge }}</span></div>
+                    <div class="lang"><i class="flag-icon" :class="this.langIcon"></i><span class="lang-txt">{{ langLangauge }}</span></div>
                 </div>
 
                 <div class="more_lang" :class="{ 'active': isActive }">
-                    <!-- <select v-model="$i18n.locale">
-                    <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
-                  </select> -->
                     <div class="lang" v-for="(l, index) in localOptions" :key="index">
                         <i class="flag-icon" :class="l.icon"></i><span class="lang-txt" @click="changeLocale(l)">{{
                             l.name }}<span>{{ l.short }} </span></span>
@@ -20,21 +16,6 @@
             </div>
         </div>
     </li>
-    <!-- <div class="translate_wrapper " :class="{'active' :isActive}">
-    <div class="current_lang">
-        <div class="lang"><i class="flag-icon" :class="this.langIcon"></i><span :class="lang-txt" @click="display()">{{ langLangauge }}</span></div>
-    </div>
-    
-        <div class="more_lang" :class="{'active' :isActive}">
-            <select v-model="$i18n.locale">
-                <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
-              </select>
-            <div class="lang" v-for="(l, index) in localOptions" :key="index">
-                 <i class="flag-icon" :class="l.icon"></i><span class="lang-txt" @click="changeLocale(l)">{{l.name}}<span>{{l.short}} </span></span>
-            </div>
-        </div>
-    
-</div> -->
 </template>
 
 <script>
